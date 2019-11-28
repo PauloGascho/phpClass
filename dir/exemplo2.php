0e9598c32de9fd540de8d38581073b19f3d15fac
PauloGascho <prgascho@gmail.com>
2019-11-28 06:07:11 -0200

    dir: comandos para criação e remoção de pasta e de informação de arquivos

---
+++ dir/exemplo2.php
@@ -0,0 +1,32 @@
+<?php
+
+$images = scandir("images");
+
+$data = array();
+
+foreach ($images as $img) {
+
+	if (!in_array($img, array(".", ".."))) {
+
+		$filename = "images" . DIRECTORY_SEPARATOR . $img;
+
+		//método de informação da pasta atribuindo o caminho indicado acima (filename) em variável
+		$info = pathinfo($filename);
+
+		//retorna tamanho do arquivo
+		$info["size"] = filesize($filename);
+
+		//retorna data de última modificação
+		$info["modified"] = date("d/m/Y H:i:s", filemtime($filename));
+
+		$info["url"] = "http://localhost/DIR/".str_replace("\\", "/", $filename);
+
+		//var_dump($info);
+
+		array_push($data, $info);
+	}
+}
+
+echo json_encode($data);
+
+?>
