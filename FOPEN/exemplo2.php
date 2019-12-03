4cfc6aa18f92d17ff35c4ade4f47e8abeda68f89
PauloGascho <prgascho@gmail.com>
2019-12-03 05:50:58 -0200

    concluindo fopen e explusão de arquivos de diretorio

--- FOPEN/exemplo2.php
+++ FOPEN/exemplo2.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("config.php")
+require_once("config.php");
 //require do arquivo de autobusca (spl_autoload_register) das classes
 
 $sql = new Sql();
@@ -14,6 +14,21 @@ $headers = array();
 foreach($usuarios[0] as $key => $value) {
     array_push($headers, ucfirst($key));
 }
+
+$file = fopen("usuarios.csv", "w+");
+
+fwrite($file, implode(",", $headers) . "\r\n");
+
+foreach ($usuarios as $row) {
+	
+	$data = array();
+
+	foreach ($row as $key => $value) {
+		array_push($data, $value);
+	}//End foreach de coluna
+
+	fwrite($file, implode(",", $data) . "\r\n");
+}//End foreach de linha
 
 print_r($headers);
 
