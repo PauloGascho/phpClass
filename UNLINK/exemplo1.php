e60673f8d8537b8fd9038aab2f3e33b68a559278
PauloGascho <prgascho@gmail.com>
2019-12-03 05:54:03 -0200

    adicionando comentários

--- UNLINK/exemplo1.php
+++ UNLINK/exemplo1.php
@@ -1,9 +1,11 @@
 <?php
 
+//função para criar o arquivo
 $file = fopen("teste.txt", "w+");
 
 fclose($file);
 
+//função para remover o arquivo
 unlink("teste.txt");
 
 echo "Arquivo removido com sucesso";
--- UNLINK/exemplo2.php
+++ UNLINK/exemplo2.php
@@ -2,6 +2,7 @@
 
 if (!is_dir("unages")) mkdir("images;");
 
+//scandir seleciona todos os arquivos de um diretorio
 foreach (scandir("images") as $item) {
 	if(!in_array($item, array(".",".."))) {
 		unlink("images/" . $item);
