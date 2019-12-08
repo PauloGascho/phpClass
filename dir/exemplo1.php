c5164727ccbb1c83beeacbc3b0f9361b4db088b5
PauloGascho <prgascho@gmail.com>
2019-12-08 08:50:43 -0300

    adicionando comentários do exemplo sobre diretórios

--- dir/exemplo1.php
+++ dir/exemplo1.php
@@ -2,11 +2,12 @@
 
 $name = "images";
 
+//verifica se o diretório não existe cria um diretório com valor da variável
 if (!is_dir($name)){
 	mkdir($name);
 	echo "Diretório $name criado com sucesso!";
+//se o diretório existe remove o diretório
 } else {
-
 	rmdir($name);
 	echo "Já existe o diretório: $name foi removido";
 }
